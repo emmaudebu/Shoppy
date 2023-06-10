ba1b09bfff0e27a60cef7fc841e4009754e74bca
ifeanyi chukwu <emmanueludebu@gmail.com>
2023-06-10 16:51:29 +0100

    done

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+
+let Bar = document.querySelector(".bx-menu")
+let menu = document.querySelector(".mobile-menu")
+
+Bar.addEventListener("click",  function show(params) {
+
+    menu.classList.toggle("show")
+   
+    
+})
